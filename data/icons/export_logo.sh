439b7350849d0f16d3d87b070ffa96c496276e68
Ippytraxx <ippytraxx@installgentoo.com>
2015-09-12 13:48:29 +0200

    Another logo update.

--- data/icons/export_logo.sh
+++ data/icons/export_logo.sh
@@ -2,5 +2,5 @@
 
 for i in 16 22 24 32 48 256 512
 do
-    inkscape -z -e $i\x$i/hicolor/apps/gnome-twitch.png -w $i -h $i gnome-twitch.svg
+    inkscape -z -e hicolor/$i\x$i/apps/gnome-twitch.png -w $i -h $i gnome-twitch.svg
 done
