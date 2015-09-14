2e51cd881b984d817c7654c601634f09224d8b74
Ippytraxx <ippytraxx@installgentoo.com>
2015-09-14 20:16:55 +0200

    Added macros for add/removing style classes.

--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,9 @@
 
 #include <gtk/gtk.h>
 
+#define REMOVE_STYLE_CLASS(w, n) gtk_style_context_remove_class(gtk_widget_get_style_context(w), n)
+#define ADD_STYLE_CLASS(w, n) gtk_style_context_add_class(gtk_widget_get_style_context(w), n)
+
 gpointer g_value_ref_sink_object(const GValue* val);
 void gtk_container_clear(GtkContainer* cont);
 gchar* g_value_dup_string_allow_null(const GValue* val);
