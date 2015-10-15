107c3db2030b6620fba682a4e8924ba5342fbf43
Ippytraxx <ippytraxx@installgentoo.com>
2015-10-15 21:51:11 +0200

    Reformat log messages.

--- src/main.c
+++ src/main.c
@@ -57,7 +57,7 @@ gt_log(const gchar* domain,
     date = g_date_time_new_now_utc();
     time_fmt = g_date_time_format(date, "%H:%M:%S");
 
-    g_print("[%s] %s - %s : '%s'\n", time_fmt, domain, level, msg);
+    g_print("[%s] %s - %s : \"%s\"\n", time_fmt, domain, level, msg);
 
     g_free(time_fmt);
     g_date_time_unref(date);
