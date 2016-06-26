a6255ce527455e34ee30e4d4d2579057a2c70ce6
Ippytraxx <ippytraxx@installgentoo.com>
2016-06-26 16:57:38 +0200

    Implement buffer-fill prop in MPV backend

--- subprojects/player-backend-mpv-opengl/gt-player-backend-mpv-opengl.c
+++ subprojects/player-backend-mpv-opengl/gt-player-backend-mpv-opengl.c
@@ -31,6 +31,7 @@ typedef struct
     gdouble volume;
     gdouble muted;
     gboolean playing;
+    gdouble buffer_fill;
 } GtPlayerBackendMpvOpenGLPrivate;
 
 static void gt_player_backend_iface_init(GtPlayerBackendInterface* iface);
@@ -46,6 +47,7 @@ enum
     PROP_PLAYING,
     PROP_MUTED,
     PROP_URI,
+    PROP_BUFFER_FILL,
     NUM_PROPS
 };
 
@@ -56,6 +58,53 @@ check_mpv_error(int status)
 {
     if (status < 0)
         g_error("{GtPlayerMpv} Mpv error %s\n", mpv_error_string(status));
+}
+
+static gboolean
+mpv_event_cb(gpointer udata)
+{
+    GtPlayerBackendMpvOpenGL* self = GT_PLAYER_BACKEND_MPV_OPENGL(udata);
+    GtPlayerBackendMpvOpenGLPrivate* priv = gt_player_backend_mpv_opengl_get_instance_private(self);
+    gboolean done = FALSE;
+
+    while (!done)
+    {
+        mpv_event* evt = mpv_wait_event(priv->mpv, 0);
+
+        switch (evt->event_id)
+        {
+            case MPV_EVENT_PROPERTY_CHANGE:
+            {
+                mpv_event_property* prop = evt->data;
+
+                if (g_strcmp0(prop->name, "volume") == 0)
+                {
+                    if (prop->data) priv->volume = *((double*) prop->data) / 100.0;
+                    g_object_notify_by_pspec(G_OBJECT(self), props[PROP_VOLUME]);
+                }
+                else if (g_strcmp0(prop->name, "cache-buffering-state") == 0)
+                {
+                    if (prop->data) priv->buffer_fill = *((gint64*) prop->data) / 100.0;
+                    g_object_notify_by_pspec(G_OBJECT(self), props[PROP_BUFFER_FILL]);
+                }
+
+                break;
+            }
+            case MPV_EVENT_NONE:
+                done = TRUE;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return G_SOURCE_REMOVE;
+}
+
+static void
+mpv_wakeup_cb(gpointer udata)
+{
+    g_idle_add(mpv_event_cb, udata);
 }
 
 static void
@@ -220,6 +269,9 @@ get_property(GObject* obj,
         case PROP_URI:
             g_value_set_string(val, priv->uri);
             break;
+        case PROP_BUFFER_FILL:
+            g_value_set_double(val, priv->buffer_fill);
+            break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, prop, pspec);
     }
@@ -252,6 +304,9 @@ set_property(GObject* obj,
             g_free(priv->uri);
             priv->uri = g_value_dup_string(val);
             break;
+        case PROP_BUFFER_FILL:
+            priv->buffer_fill = g_value_get_double(val);
+            break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, prop, pspec);
     }
@@ -265,6 +320,7 @@ finalise(GObject* obj)
 
     g_message("{GtPlayerBackendMpvOpenGL} Finalise");
 
+    mpv_set_wakeup_callback(priv->mpv, NULL, NULL);
     mpv_opengl_cb_set_update_callback(priv->mpv_opengl, NULL, NULL);
     mpv_opengl_cb_uninit_gl(priv->mpv_opengl);
     mpv_terminate_destroy(priv->mpv);
@@ -316,10 +372,17 @@ gt_player_backend_mpv_opengl_class_init(GtPlayerBackendMpvOpenGLClass* klass)
                                           "Current uri",
                                           NULL,
                                           G_PARAM_READWRITE);
+    props[PROP_BUFFER_FILL] = g_param_spec_double("buffer-fill",
+                                                     "Buffer Fill",
+                                                     "Current buffer fill",
+                                                     0, 1.0, 0,
+                                                     G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+
     g_object_class_override_property(obj_class, PROP_VOLUME, "volume");
     g_object_class_override_property(obj_class, PROP_MUTED, "muted");
     g_object_class_override_property(obj_class, PROP_PLAYING, "playing");
     g_object_class_override_property(obj_class, PROP_URI, "uri");
+    g_object_class_override_property(obj_class, PROP_BUFFER_FILL, "buffer-fill");
 }
 
 static void
@@ -336,8 +399,15 @@ gt_player_backend_mpv_opengl_init(GtPlayerBackendMpvOpenGL* self)
 
     g_object_set(priv->widget, "expand", TRUE, NULL);
 
+    check_mpv_error(mpv_set_option_string(priv->mpv, "audio-client-name", "GNOME Twitch"));
+    check_mpv_error(mpv_set_option_string(priv->mpv, "title", ""));
     check_mpv_error(mpv_set_option_string(priv->mpv, "vo", "opengl-cb"));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "volume", MPV_FORMAT_DOUBLE));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "cache-buffering-state", MPV_FORMAT_INT64));
+
     check_mpv_error(mpv_initialize(priv->mpv));
+
+    mpv_set_wakeup_callback(priv->mpv, mpv_wakeup_cb, self);
 
     priv->mpv_opengl = mpv_get_sub_api(priv->mpv, MPV_SUB_API_OPENGL_CB);
 
