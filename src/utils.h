a6f814e3c3fe1729c7ae501534492c82d587b7a4
Ippytraxx <ippytraxx@installgentoo.com>
2016-07-04 21:45:42 +0200

    Add easy to use logging funcs

--- src/utils.h
+++ src/utils.h
@@ -9,6 +9,21 @@
 #define UROUND(x) ((guint) ((x) + 0.5))
 #define PRINT_BOOL(b) b ? "true" : "false"
 
+#ifndef TAG
+#error Tag not defined
+#else
+#define LOG(lvl, msg) g_log(NULL, lvl, "{%s} %s", TAG, msg)
+#define LOGF(lvl, fmt, ...) g_log(NULL, lvl, "{%s} " fmt, TAG, __VA_ARGS__)
+#define WARNING(msg) LOG(G_LOG_LEVEL_WARNING, msg)
+#define WARNINGF(fmt, ...) LOGF(G_LOG_LEVEL_WARNING, fmt, __VA_ARGS__)
+#define MESSAGE(msg) LOG(G_LOG_LEVEL_MESSAGE, msg)
+#define MESSAGEF(fmt, ...) LOG(G_LOG_LEVEL_MESSAGE, fmt, __VA_ARGS__)
+#define INFO(msg) LOG(G_LOG_LEVEL_INFO, msg)
+#define INFOF(fmt, ...) LOG(G_LOG_LEVEL_INFO, fmt, __VA_ARGS__)
+#define DEBUG(msg) LOG(G_LOG_LEVEL_DEBUG, msg)
+#define DEBUGF(fmt, ...) LOG(G_LOG_LEVEL_DEBUG, fmt, __VA_ARGS__)
+#endif
+
 gpointer utils_value_ref_sink_object(const GValue* val);
 gchar* utils_value_dup_string_allow_null(const GValue* val);
 void utils_container_clear(GtkContainer* cont);
