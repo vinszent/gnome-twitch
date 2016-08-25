d3990b3a7ccbe63251ebf35c74cc9d2a6b385735
Ippytraxx <ippytraxx@installgentoo.com>
2016-08-26 00:01:51 +0200

    Prepare for 0.3.0 translation

--- src/gt-app.c
+++ src/gt-app.c
@@ -342,6 +342,7 @@ startup(GApplication* app)
     menu_bld = gtk_builder_new_from_resource("/com/vinszent/GnomeTwitch/ui/app-menu.ui");
     priv->app_menu = G_MENU_MODEL(gtk_builder_get_object(menu_bld, "app_menu"));
     g_object_ref(priv->app_menu);
+    priv->login_item = g_menu_item_new(_("Login to Twitch"), "win.show_twitch_login");
     g_menu_prepend_item(G_MENU(priv->app_menu), priv->login_item);
 
     gtk_application_set_app_menu(GTK_APPLICATION(app), G_MENU_MODEL(priv->app_menu));
@@ -505,9 +506,6 @@ gt_app_init(GtApp* self)
 //    ADD_PLUGINS_PATH(g_get_user_data_dir(), "recorder-backends");
 
 #undef ADD_PLUGINS_PATH
-
-    priv->login_item = g_menu_item_new(_("Login to Twitch"), "win.show_twitch_login");
-
 
     g_settings_bind(self->settings, "loaded-plugins",
                     self->plugins_engine, "loaded-plugins",
