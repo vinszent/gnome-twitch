9775ca7300aab12b79c7d78e7236545bed02800b
vinszent <vinszent@vinszent.com>
2017-02-27 19:19:14 +0100

    Add a license header to most files

--- src/gt-win.c
+++ src/gt-win.c
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of GNOME Twitch - 'Enjoy Twitch on your GNU/Linux desktop'
+ *  Copyright © 2017 Vincent Szolnoky <vinszent@vinszent.com>
+ *
+ *  GNOME Twitch is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GNOME Twitch is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNOME Twitch. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "gt-win.h"
 #include <glib/gprintf.h>
 #include <glib/gi18n.h>
@@ -127,7 +145,7 @@ show_about_cb(GSimpleAction* action,
                  "program-name", "GNOME Twitch",
                  "authors", &authors,
                  "license-type", GTK_LICENSE_GPL_3_0,
-                 "copyright", "Copyright © 2015 Vincent Szolnoky",
+                 "copyright", "Copyright © 2017 Vincent Szolnoky",
                  "comments", _("Enjoy Twitch on your GNU/Linux desktop"),
                  "logo-icon-name", "com.vinszent.GnomeTwitch",
                  "website", "https://github.com/vinszent/gnome-twitch",
