bd51e82207e07517c2e8b24cd0ff790ebabb7cac
Patrick Griffis <tingping@tingping.se>
2017-03-09 09:07:58 -0500

    build: Replace shell with python
    This is more portable since meson already requires it

--- print_git_version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if test "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = "true"; then
-    printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
-else
-    exit 1
-fi;
