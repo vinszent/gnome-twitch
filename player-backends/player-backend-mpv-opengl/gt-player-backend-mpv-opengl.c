ffecf9f1f9241ceba5a246c0cf658d32ea4e8a70
vinszent <vinszent@vinszent.com>
2018-01-26 22:56:45 +0100

    Implement new props & funcs in MPV OpenGL backend

--- player-backends/player-backend-mpv-opengl/gt-player-backend-mpv-opengl.c
+++ player-backends/player-backend-mpv-opengl/gt-player-backend-mpv-opengl.c
@@ -48,31 +48,70 @@ typedef struct
     GtkWidget* widget;
 
     gchar* uri;
-
     gdouble volume;
-    gboolean playing;
     gdouble buffer_fill;
+    gboolean seekable;
+    gint64 duration;
+    gint64 position;
+    GtPlayerBackendState state;
 
     gint mpv_event_cb_id;
 } GtPlayerBackendMpvOpenGLPrivate;
 
 static void gt_player_backend_iface_init(GtPlayerBackendInterface* iface);
 
-G_DEFINE_DYNAMIC_TYPE_EXTENDED(GtPlayerBackendMpvOpenGL, gt_player_backend_mpv_opengl, PEAS_TYPE_EXTENSION_BASE, 0,
-                               G_IMPLEMENT_INTERFACE_DYNAMIC(GT_TYPE_PLAYER_BACKEND, gt_player_backend_iface_init)
-                               G_ADD_PRIVATE_DYNAMIC(GtPlayerBackendMpvOpenGL))
+G_DEFINE_DYNAMIC_TYPE_EXTENDED(GtPlayerBackendMpvOpenGL, gt_player_backend_mpv_opengl,
+    PEAS_TYPE_EXTENSION_BASE, 0,
+    G_IMPLEMENT_INTERFACE_DYNAMIC(GT_TYPE_PLAYER_BACKEND, gt_player_backend_iface_init)
+    G_ADD_PRIVATE_DYNAMIC(GtPlayerBackendMpvOpenGL))
 
 enum
 {
     PROP_0,
     PROP_VOLUME,
-    PROP_PLAYING,
-    PROP_URI,
     PROP_BUFFER_FILL,
-    NUM_PROPS
+    PROP_DURATION,
+    PROP_POSITION,
+    PROP_SEEKABLE,
+    PROP_STATE,
+    NUM_PROPS,
 };
 
 static GParamSpec* props[NUM_PROPS];
+
+/* TODO: Get rid of this and move utils stuff into a shared place */
+static void
+weak_ref_notify_cb(gpointer udata, GObject* obj) /* NOTE: This object is no longer valid, can only use the address */
+{
+    RETURN_IF_FAIL(udata != NULL);
+
+    g_weak_ref_set(udata, NULL);
+}
+
+GWeakRef*
+utils_weak_ref_new(gpointer obj)
+{
+    GWeakRef* ref = g_malloc(sizeof(GWeakRef));
+
+    g_weak_ref_init(ref, obj);
+    g_object_weak_ref(obj, weak_ref_notify_cb, ref);
+
+    return ref;
+}
+
+void
+utils_weak_ref_free(GWeakRef* ref)
+{
+    g_autoptr(GObject) obj = g_weak_ref_get(ref);
+
+    if (obj)
+        g_object_weak_unref(obj, weak_ref_notify_cb, ref);
+
+    g_weak_ref_clear(ref);
+    g_free(ref);
+}
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GWeakRef, utils_weak_ref_free);
 
 static inline void
 check_mpv_error(int status)
@@ -80,6 +119,35 @@ check_mpv_error(int status)
     if (status < 0)
         ERRORF("Mpv error %s\n", mpv_error_string(status));
 }
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GParamSpec, g_param_spec_unref);
+
+/* NOTE: This is so that we notify properies on the GLib main thread,
+  * otherwise bad things can happen */
+static gboolean
+notify_prop_cb(gpointer udata)
+{
+    RETURN_VAL_IF_FAIL(G_IS_PARAM_SPEC(udata), G_SOURCE_REMOVE);
+
+    GParamSpec* pspec = udata; /* NOTE: Will be destroyed automatically by idle_add_full */
+    g_autoptr(GWeakRef) ref = g_param_spec_steal_qdata(pspec, g_quark_from_string("_gt_self"));
+    g_autoptr(GtPlayerBackendMpvOpenGL) self = g_weak_ref_get(ref);
+
+    if (!self) {TRACE("Unreffed while wating"); return G_SOURCE_REMOVE;}
+
+    g_object_notify_by_pspec(G_OBJECT(self), pspec);
+
+    return G_SOURCE_REMOVE;
+}
+
+#define NOTIFY_PROP(self, pprop)                                         \
+    G_STMT_START                                                        \
+    {                                                                   \
+        g_param_spec_set_qdata_full(pprop, g_quark_from_string("_gt_self"), \
+            utils_weak_ref_new(self), (GDestroyNotify) utils_weak_ref_free); \
+        g_idle_add_full(G_PRIORITY_DEFAULT, notify_prop_cb,             \
+            g_param_spec_ref(pprop), (GDestroyNotify) g_param_spec_unref); \
+    } G_STMT_END
 
 static gboolean
 mpv_event_cb(gpointer udata)
@@ -101,14 +169,49 @@ mpv_event_cb(gpointer udata)
                 if (g_strcmp0(prop->name, "volume") == 0)
                 {
                     if (prop->data) priv->volume = *((double*) prop->data) / 100.0;
-                    g_object_notify_by_pspec(G_OBJECT(self), props[PROP_VOLUME]);
+                    NOTIFY_PROP(self, props[PROP_VOLUME]);
+
                 }
                 else if (g_strcmp0(prop->name, "cache-buffering-state") == 0)
                 {
                     if (prop->data) priv->buffer_fill = *((gint64*) prop->data) / 100.0;
-                    g_object_notify_by_pspec(G_OBJECT(self), props[PROP_BUFFER_FILL]);
+                    NOTIFY_PROP(self, props[PROP_BUFFER_FILL]);
                 }
-
+                else if (g_strcmp0(prop->name, "time-pos") == 0)
+                {
+                    if (prop->data) priv->position = *((gint64*) prop->data);
+                    NOTIFY_PROP(self, props[PROP_POSITION]);
+                }
+                else if (g_strcmp0(prop->name, "duration") == 0)
+                {
+                    /* NOTE: The seekable prop isn't very good because
+                     * it still returns true for a live HLS stream */
+                    if (prop->data)
+                    {
+                        priv->duration = *((gint64*) prop->data);
+                        priv->seekable = priv->duration > 0;
+                    }
+                    NOTIFY_PROP(self, props[PROP_DURATION]);
+                    NOTIFY_PROP(self, props[PROP_SEEKABLE]);
+                }
+                else if (g_strcmp0(prop->name, "pause") == 0)
+                {
+                }
+                else if (g_strcmp0(prop->name, "unpause") == 0)
+                {
+                }
+                else if (g_strcmp0(prop->name, "core-idle") == 0)
+                {
+                }
+                else if (g_strcmp0(prop->name, "playback-abort") == 0)
+                {
+                    if (prop->data)
+                    {
+                        priv->state = *((gboolean*) prop->data) ?
+                            GT_PLAYER_BACKEND_STATE_STOPPED : GT_PLAYER_BACKEND_STATE_PLAYING;
+                    }
+                    NOTIFY_PROP(self, props[PROP_STATE]);
+                }
                 break;
             }
             case MPV_EVENT_NONE:
@@ -134,8 +237,11 @@ mpv_wakeup_cb(gpointer udata)
 }
 
 static void
-play(GtPlayerBackendMpvOpenGL* self)
+play(GtPlayerBackend* backend)
 {
+    RETURN_IF_FAIL(GT_IS_PLAYER_BACKEND_MPV_OPENGL(backend));
+
+    GtPlayerBackendMpvOpenGL* self = GT_PLAYER_BACKEND_MPV_OPENGL(backend);
     GtPlayerBackendMpvOpenGLPrivate* priv = gt_player_backend_mpv_opengl_get_instance_private(self);
 
     if (priv->uri)
@@ -143,17 +249,30 @@ play(GtPlayerBackendMpvOpenGL* self)
         const gchar* mpv_cmd[] = {"loadfile", priv->uri, "replace", NULL};
 
         check_mpv_error(mpv_command(priv->mpv, mpv_cmd));
+
+        priv->state = GT_PLAYER_BACKEND_STATE_LOADING;
+        g_object_notify_by_pspec(G_OBJECT(self), props[PROP_STATE]);
     }
 }
 
 static void
-stop(GtPlayerBackendMpvOpenGL* self)
+stop(GtPlayerBackend* backend)
 {
+    RETURN_IF_FAIL(GT_IS_PLAYER_BACKEND_MPV_OPENGL(backend));
+
+    GtPlayerBackendMpvOpenGL* self = GT_PLAYER_BACKEND_MPV_OPENGL(backend);
     GtPlayerBackendMpvOpenGLPrivate* priv = gt_player_backend_mpv_opengl_get_instance_private(self);
 
     const gchar* mpv_cmd[] = {"stop", NULL};
 
     check_mpv_error(mpv_command(priv->mpv, mpv_cmd));
+}
+
+/* NOTE: This is so dumb but unistd.h already defines pause() */
+static void
+_pause(GtPlayerBackend* backend)
+{
+    /* TODO: Implement */
 }
 
 static void
@@ -164,6 +283,33 @@ set_volume(GtPlayerBackendMpvOpenGL* self, gdouble volume)
     gdouble vol = volume*100.0;
 
     mpv_set_property(priv->mpv, "volume", MPV_FORMAT_DOUBLE, &vol);
+}
+
+static void
+set_uri(GtPlayerBackend* backend, const gchar* uri)
+{
+    RETURN_IF_FAIL(GT_IS_PLAYER_BACKEND_MPV_OPENGL(backend));
+
+    GtPlayerBackendMpvOpenGL* self = GT_PLAYER_BACKEND_MPV_OPENGL(backend);
+    GtPlayerBackendMpvOpenGLPrivate* priv = gt_player_backend_mpv_opengl_get_instance_private(self);
+
+    g_free(priv->uri);
+    priv->uri = g_strdup(uri);
+}
+
+static void
+set_position(GtPlayerBackend* backend, gint64 position)
+{
+    RETURN_IF_FAIL(GT_IS_PLAYER_BACKEND_MPV_OPENGL(backend));
+
+    GtPlayerBackendMpvOpenGL* self = GT_PLAYER_BACKEND_MPV_OPENGL(backend);
+    GtPlayerBackendMpvOpenGLPrivate* priv = gt_player_backend_mpv_opengl_get_instance_private(self);
+
+    g_autofree gchar* seconds = g_strdup_printf("%ld", position);
+
+    const gchar* mpv_cmd[] = {"seek", seconds, "absolute", NULL};
+
+    check_mpv_error(mpv_command(priv->mpv, mpv_cmd));
 }
 
 static GtkWidget*
@@ -279,14 +425,20 @@ get_property(GObject* obj,
         case PROP_VOLUME:
             g_value_set_double(val, priv->volume);
             break;
-        case PROP_PLAYING:
-            g_value_set_boolean(val, priv->playing);
-            break;
-        case PROP_URI:
-            g_value_set_string(val, priv->uri);
-            break;
         case PROP_BUFFER_FILL:
             g_value_set_double(val, priv->buffer_fill);
+            break;
+        case PROP_DURATION:
+            g_value_set_int64(val, priv->duration);
+            break;
+        case PROP_POSITION:
+            g_value_set_int64(val, priv->position);
+            break;
+        case PROP_SEEKABLE:
+            g_value_set_boolean(val, priv->seekable);
+            break;
+        case PROP_STATE:
+            g_value_set_enum(val, priv->state);
             break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, prop, pspec);
@@ -307,17 +459,6 @@ set_property(GObject* obj,
         case PROP_VOLUME:
             priv->volume = g_value_get_double(val);
             set_volume(self, priv->volume);
-            break;
-        case PROP_PLAYING:
-            priv->playing = g_value_get_boolean(val);
-            priv->playing ? play(self) : stop(self);
-            break;
-        case PROP_URI:
-            g_free(priv->uri);
-            priv->uri = g_value_dup_string(val);
-            break;
-        case PROP_BUFFER_FILL:
-            priv->buffer_fill = g_value_get_double(val);
             break;
         default:
             G_OBJECT_WARN_INVALID_PROPERTY_ID(obj, prop, pspec);
@@ -355,6 +496,12 @@ static void
 gt_player_backend_iface_init(GtPlayerBackendInterface* iface)
 {
     iface->get_widget = get_widget;
+    iface->play = play;
+    iface->stop = stop;
+    iface->pause = _pause;
+    iface->set_uri = set_uri;
+    iface->set_position = set_position;
+
 }
 
 static void
@@ -366,31 +513,30 @@ gt_player_backend_mpv_opengl_class_init(GtPlayerBackendMpvOpenGLClass* klass)
     obj_class->get_property = get_property;
     obj_class->set_property = set_property;
 
-    props[PROP_VOLUME] = g_param_spec_double("volume",
-                                             "Volume",
-                                             "Volume of player",
-                                             0.0, 1.0, 0.3,
-                                             G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
-    props[PROP_PLAYING] = g_param_spec_boolean("playing",
-                                               "Playing",
-                                               "Whether playing",
-                                               FALSE,
-                                               G_PARAM_READABLE | G_PARAM_CONSTRUCT);
-    props[PROP_URI] = g_param_spec_string("uri",
-                                          "Uri",
-                                          "Current uri",
-                                          NULL,
-                                          G_PARAM_READWRITE);
-    props[PROP_BUFFER_FILL] = g_param_spec_double("buffer-fill",
-                                                     "Buffer Fill",
-                                                     "Current buffer fill",
-                                                     0, 1.0, 0,
-                                                     G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+    props[PROP_VOLUME] = g_param_spec_double("volume", "Volume", "Volume of player",
+        0.0, 1.0, 0.3, G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_CONSTRUCT);
+
+    props[PROP_BUFFER_FILL] = g_param_spec_double("buffer-fill", "Buffer Fill", "Current buffer fill",
+        0, 1.0, 0, G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+
+    props[PROP_DURATION] = g_param_spec_int64("duration", "Duration", "Current duration",
+        0, G_MAXINT64, 0, G_PARAM_READABLE);
+
+    props[PROP_POSITION] = g_param_spec_int64("position", "Position", "Current position",
+        0, G_MAXINT64, 0, G_PARAM_READWRITE);
+
+    props[PROP_SEEKABLE] = g_param_spec_boolean("seekable", "Seekable", "Whether seekable",
+        FALSE, G_PARAM_READABLE);
+
+    props[PROP_STATE] = g_param_spec_enum("state", "State", "Current state",
+        GT_TYPE_PLAYER_BACKEND_STATE, GT_PLAYER_BACKEND_STATE_STOPPED, G_PARAM_READABLE);
 
     g_object_class_override_property(obj_class, PROP_VOLUME, "volume");
-    g_object_class_override_property(obj_class, PROP_PLAYING, "playing");
-    g_object_class_override_property(obj_class, PROP_URI, "uri");
     g_object_class_override_property(obj_class, PROP_BUFFER_FILL, "buffer-fill");
+    g_object_class_override_property(obj_class, PROP_DURATION, "duration");
+    g_object_class_override_property(obj_class, PROP_POSITION, "position");
+    g_object_class_override_property(obj_class, PROP_SEEKABLE, "seekable");
+    g_object_class_override_property(obj_class, PROP_STATE, "state");
 }
 
 static void
@@ -401,6 +547,10 @@ gt_player_backend_mpv_opengl_init(GtPlayerBackendMpvOpenGL* self)
     MESSAGE("Init");
 
     setlocale(LC_NUMERIC, "C");
+
+    priv->uri = NULL;
+    priv->state = GT_PLAYER_BACKEND_STATE_STOPPED;
+    priv->buffer_fill = 0.0;
 
     priv->widget = gtk_gl_area_new();
     priv->mpv = mpv_create();
@@ -416,6 +566,12 @@ gt_player_backend_mpv_opengl_init(GtPlayerBackendMpvOpenGL* self)
     check_mpv_error(mpv_set_option_string(priv->mpv, "softvol-max", "100"));
     check_mpv_error(mpv_observe_property(priv->mpv, 0, "volume", MPV_FORMAT_DOUBLE));
     check_mpv_error(mpv_observe_property(priv->mpv, 0, "cache-buffering-state", MPV_FORMAT_INT64));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "time-pos", MPV_FORMAT_INT64));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "duration", MPV_FORMAT_INT64));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "pause", MPV_FORMAT_FLAG));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "unpause", MPV_FORMAT_FLAG));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "core-idle", MPV_FORMAT_FLAG));
+    check_mpv_error(mpv_observe_property(priv->mpv, 0, "playback-abort", MPV_FORMAT_FLAG));
 
     check_mpv_error(mpv_initialize(priv->mpv));
 
@@ -437,6 +593,5 @@ peas_register_types(PeasObjectModule* module)
     gt_player_backend_mpv_opengl_register_type(G_TYPE_MODULE(module));
 
     peas_object_module_register_extension_type(module,
-                                               GT_TYPE_PLAYER_BACKEND,
-                                               GT_TYPE_PLAYER_BACKEND_MPV_OPENGL);
+        GT_TYPE_PLAYER_BACKEND, GT_TYPE_PLAYER_BACKEND_MPV_OPENGL);
 }
